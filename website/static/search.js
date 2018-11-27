e8c6d8cd9fd5ec2e9e9741592fb5a04c524d7cfd
Benjamin Modayil <modayilme@gmail.com>
2018-11-27 12:22:03 -0500

    website search + parcel

---
+++ website/static/search.js
@@ -0,0 +1,42 @@
+const search = instantsearch({
+  appId: process.env.ALGOLIA_APP_ID,
+  apiKey: process.env.ALGOLIA_ADMIN_KEY,
+  indexName: 'apprenticeships',
+  routing: true,
+  searchableAttributes: ["company", "description", "location"],
+  searchFunction: function(helper) {
+    helper.search();
+  }
+})
+
+// initialize hits widge
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      allItems: `
+        <ul>
+          {{#hits}}
+            <li>
+              <a class="post post-entry" href="{{{link}}}" target="_blank">
+                <h2 class="post-header">{{{company}}}</h2>
+                <p class="post__location">{{{location}}}</p>
+                <p>{{{description}}}</p>
+              </a>
+            </li>
+          {{/hits}}
+        </ul>
+    `,
+    empty: 'No results for {{query}}'
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search__box',
+    placeholder: 'Search for apprenticeships'
+  })
+);
+
+search.start()

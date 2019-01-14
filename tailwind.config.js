4101d1d8e373a99b186471feb07b7f574610df47
Frances Coronel <hello@fvcproductions.com>
2019-01-13 21:39:45 -0800

    remove extra font weights
    Signed-off-by: Frances Coronel <hello@fvcproductions.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -176,15 +176,8 @@ module.exports = {
   */
 
   fontWeights: {
-    hairline: 100,
-    thin: 200,
-    light: 300,
     normal: 400,
-    medium: 500,
-    semibold: 600,
     bold: 700,
-    extrabold: 800,
-    black: 900
   },
 
   /*

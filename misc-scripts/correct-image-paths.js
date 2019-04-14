b47de2dc11211bc113d40c3381a33695d3ed36ae
Benjamin Modayil <modayilme@gmail.com>
2019-04-14 09:28:05 -0400

    tweak eslint for async + reduce block nesting

--- misc-scripts/correct-image-paths.js
+++ misc-scripts/correct-image-paths.js
@@ -1,70 +1,62 @@
 const fs = require("fs");
+const {promisify} = require("util");
 const path = require("path");
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+
 const matchObj = [];
 
-const findDistMatch = () => {
-  return new Promise(function(resolve, reject) {
-    fs.readdir(path.resolve(__dirname, "../dist"), function(err, flist) {
-      if (err) {
-        console.log("Error reading directory ");
-        console.log(err);
-        return;
-      }
-      const matches = flist.map((apprenticeship) => {
-        const file = apprenticeship.split(".");
-        const name = file[0];
-        const hash = file[1];
-        const type = file[2];
-        const match = matchObj.map((i) => {
-          if (i.name === name && i.type === type) {
-            i.hash = hash;
-          }
-        });
+const findDistMatch = async() => {
+  const directory = await readDir(path.resolve(__dirname, "../dist"));
 
-        return match[0];
-      });
-      resolve(matches);
-      return matches;
+  return directory.map((file) => {
+    const fileName = file.split(".");
+    const name = fileName[0];
+    const hash = fileName[1];
+    const type = fileName[2];
+    const match = matchObj.map((i) => {
+      if (i.name === name && i.type === type) {
+        i.hash = hash;
+      }
     });
+
+    return match[0];
   });
 };
 
-fs.readFile(path.resolve(__dirname, "../public/algolia.json"), function read(
-  err,
-  data
-) {
-  if (err) {
-    throw err;
-  }
-  content = JSON.parse(data);
-  content.map((i, index) => {
+const addCorrectImageToJSONFile = (content) => {
+  return content.map((x) => {
+    const fileName = x.image.split(".");
+    const name = fileName[0];
+    const type = fileName[1];
+
+    matchObj.map((b) => {
+      if (b.name === name && b.type === type) {
+        x.image = `${b.name}.${b.hash}.${b.type}`;
+      }
+    });
+
+    return x.image;
+  });
+};
+
+const initScript = async() => {
+  const data = readFile(path.resolve(__dirname, "../public/algolia.json"));
+  content = JSON.parse(await data);
+  content.map((i) => {
     const file = i.image.split(".");
     const name = file[0];
     const type = file[1];
     matchObj.push({name, type});
     return i.image;
   });
-  findDistMatch()
-    .then(() => {
-      content.map((x) => {
-        const file = x.image.split(".");
-        const name = file[0];
-        const type = file[1];
 
-        const match = matchObj.map((b) => {
-          if (b.name === name && b.type === type) {
-            x.image = `${b.name}.${b.hash}.${b.type}`;
-          }
-        });
+  await findDistMatch();
+  addCorrectImageToJSONFile(content);
+  fs.writeFile(
+    path.resolve(__dirname, "../public/algolia.json"),
+    JSON.stringify(content)
+  );
+};
 
-        return x.image;
-      });
-    })
-    .then(() => {
-      fs.writeFile(
-        path.resolve(__dirname, "../public/algolia.json"),
-        JSON.stringify(content),
-        () => console.log("success")
-      );
-    });
-});
+initScript();

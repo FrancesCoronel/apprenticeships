66a49fa9b410ee00951266202425896e45b2b171
Benjamin Modayil <modayilme@gmail.com>
2019-04-14 20:34:24 -0400

    edited callback

--- misc-scripts/lambda/form.js
+++ misc-scripts/lambda/form.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 const {GH_ACCESS_TOKEN} = process.env;
 
-const API_ENDPOINT =
-  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
+// const API_ENDPOINT =
+//   "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
 
 exports.handler = async(event, context) => {
   // return fetch(API_ENDPOINT)
@@ -12,7 +12,10 @@ exports.handler = async(event, context) => {
   //     body: `${data.setup} ${data.punchline} *BA DUM TSSS*`
   //   }))
   //   .catch((error) => ({statusCode: 422, body: String(error)}));
-  return "hello-world";
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  });
 };
 
 // User fills out form, form hits this function, function calls a process.env.access_token for GH permissions

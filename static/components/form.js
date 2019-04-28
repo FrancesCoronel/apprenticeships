f77f7a57a00de08315bf08f71dfa6fb151f89dde
Benjamin Modayil <modayilme@gmail.com>
2019-04-28 09:37:41 -0400

    check for captcha on the front-end

--- static/components/form.js
+++ static/components/form.js
@@ -9,7 +9,6 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const captcha = form.querySelector("#g-recaptcha-response");
   const URL = "/.netlify/functions/form";
-
   const issue = {
     title: title.value.trim(),
     body:
@@ -21,32 +20,34 @@ form.addEventListener("submit", (e) => {
     labels: ["enhancement", "help wanted"],
     captcha: captcha.value
   };
+  const paragraph = document.createElement("p");
+  const classes = ["text-red", "text-base", "mb-6", "text-center"];
+  paragraph.classList.add(...classes);
+  form.prepend(paragraph);
 
-  return fetch(URL, {
-    method: "POST",
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(issue)
-  })
-    .then((i) => {
-      console.log(i);
-      if (i.status !== 200) throw Error();
-      console.log(i);
-      return i;
+  if (grecaptcha && grecaptcha.getResponse().length > 0) {
+    return fetch(URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(issue)
     })
-    .then(() => {
-      formParent.innerHTML = `
+      .then((i) => {
+        if (i.status !== 200) throw Error();
+        return i;
+      })
+      .then(() => {
+        formParent.innerHTML = `
       <p>Thank you for your contribution!</p>
       `;
-    })
-    .catch((err) => {
-      const paragraph = document.createElement("p");
-      const classes = ["text-red", "text-base", "mb-6", "text-center"];
-      paragraph.classList.add(...classes);
-      paragraph.innerHTML = `Oh no, something went wrong. <br> Please try again or enter it directly <a class="font-bold text-green-dark hover-text-green-darker no-underline hover-underline transition"
+      })
+      .catch((err) => {
+        paragraph.innerHTML = `Oh no, something went wrong. <br> Please try again or enter it directly <a class="font-bold text-green-dark hover-text-green-darker no-underline hover-underline transition"
         href="https://github.com/fvcproductions/apprenticeships.me/issues/new/choose">here</a>.`;
-      form.prepend(paragraph);
-    });
+      });
+  } else {
+    paragraph.innerHTML = "Oops, you have to check the recaptcha!";
+  }
 });

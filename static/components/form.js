33f48b4d2f6049aed0be4147631afea5cc00e24a
Benjamin Modayil <modayilme@gmail.com>
2019-04-30 20:18:46 -0400

    form loading state added

--- static/components/form.js
+++ static/components/form.js
@@ -4,6 +4,8 @@ const title = form.querySelector("#title");
 const link = form.querySelector("#link");
 const description = form.querySelector("#description");
 const locations = form.querySelector("#locations");
+const formButton = document.querySelector(".gh-form__button");
+const loader = document.querySelector(".icon-loader");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -61,7 +63,17 @@ ${locations.value.trim()}
       body: JSON.stringify(issue)
     })
       .then((i) => {
-        if (i.status !== 200) throw Error();
+        formButton.classList.add("hidden");
+        loader.classList.remove("hidden");
+        return i;
+      })
+      .then((i) => {
+        if (i.status !== 200) {
+          throw Error();
+        } else {
+          formButton.classList.remove("hidden");
+          loader.classList.add("hidden");
+        }
         return i;
       })
       .then(() => {
@@ -70,6 +82,7 @@ ${locations.value.trim()}
       `;
       })
       .catch((err) => {
+        formButton.classList.remove("hidden");
         paragraph.innerHTML = `Oh no, something went wrong. <br> Please try again or enter it directly <a class="font-bold text-green-dark hover-text-green-darker no-underline hover-underline transition"
         href="https://github.com/fvcproductions/apprenticeships.me/issues/new/choose">here</a>.`;
       });
